fd142b1f29e7a14ff08175a8140e5d26e4f8f005
skopaliks <skopalik@elektlabs.cz>
2017-09-03 20:20:39 +0200

    bat file for covering install

---
+++ AddToDB.bat
@@ -0,0 +1,15 @@
+:: This script add or update SQL library objects to specific DB
+:: %1 - database name like localhost:d:\fbdata\db1.fdb
+
+:: if you don't have isql.exe in PATH then set ISQL enviroment variable like c:\fb\bin\isql.exe
+
+:: for user name and password is used ISC_USER and ISC_PASSWORD enviroment variables
+
+IF NOT DEFINED %ISQL% SET ISQL=isql.exe
+
+@PUSHD .
+@CD /D %~dp0
+
+%ISQL% -b -e -q -charset UTF8 -i IncludeAll.sql %1
+
+@POPD
