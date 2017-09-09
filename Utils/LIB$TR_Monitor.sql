7826e83a540588b0c354ba24a72657ada7405f3b
skopaliks <skopalik@elektlabs.cz>
2017-09-09 12:51:25 +0200

    TR_Monitor - Final version

--- Utils/LIB$TR_Monitor.sql
+++ Utils/LIB$TR_Monitor.sql
@@ -11,7 +11,8 @@
 ******************************************************************************/
 
 RECREATE Table LIB$Transactions_Log (
-	tDateUTC            Lib$TimestampUTC NOT NULL,
+	bDateUTC            Lib$TimestampUTC NOT NULL,  -- When transaction start
+  eDateUTC            Lib$TimestampUTC NOT NULL,  -- When transaction finished
   RollBacked          SMALLINT         NOT NULL,
 	Log_REMOTE_PROTOCOL VARCHAR(10)      NOT NULL,
   Log_REMOTE_ADDRESS  VARCHAR(255)     NOT NULL,
@@ -21,24 +22,12 @@ RECREATE Table LIB$Transactions_Log (
 	Log_TRANSACTION_ID  BIGINT           NOT NULL,
 	Log_ISOLATION_Mode  SMALLINT         NOT NULL,
 	Log_REMOTE_PROCESS  VARCHAR(255),
-	usr_msg             VARCHAR(511)
+	usr_msg             VARCHAR(511)                -- For debuging, use context variable 'USER_TRANSACTION'.'LIB$Transactions_Log_usr_msg'
 );
 
 GRANT INSERT ON LIB$Transactions_Log TO PUBLIC;
 
 SET TERM ^;
-
-CREATE OR ALTER TRIGGER LIB$Transactions_Log_BI FOR MON_Transactions
-ACTIVE BEFORE INSERT POSITION 2
-AS
-BEGIN
-  -- Just to pass Elekt Labs replication check
-  -- IF(Rdb$Get_Context(''USER_SESSION'',''DatabaseReplicationFlag'') IS NOT NULL) THEN EXIT;
-  new.tDateUTC = GetExactTimestampUTC();
-  new.Log_REMOTE_PROTOCOL = RDB$GET_CONTEXT('SYSTEM', 'NETWORK_PROTOCOL');
-  new.Log_REMOTE_ADDRESS = RDB$GET_CONTEXT('SYSTEM', 'CLIENT_ADDRESS');
-END
-
 
 EXECUTE BLOCK AS
 DECLARE ds VARCHAR(500);
@@ -52,5 +41,53 @@ BEGIN
 END
 ^
 
+CREATE OR ALTER TRIGGER LIB$Transactions_Log_BI FOR LIB$Transactions_Log
+ACTIVE BEFORE INSERT POSITION 2
+AS
+BEGIN
+  -- Just to pass Elekt Labs replication check
+  -- IF(Rdb$Get_Context('USER_SESSION','DatabaseReplicationFlag') IS NOT NULL) THEN EXIT;
+  new.bDateUTC            = RDB$Get_Context('USER_TRANSACTION','LIB$Transactions_Log_bDateUTC'); 
+  new.eDateUTC            = GetExactTimestampUTC();
+  new.Log_REMOTE_PROTOCOL = RDB$GET_CONTEXT('SYSTEM', 'NETWORK_PROTOCOL');
+  new.Log_REMOTE_ADDRESS  = RDB$GET_CONTEXT('SYSTEM', 'CLIENT_ADDRESS');
+  new.Log_CURRENT_USER    = CURRENT_USER;
+  new.Log_CURRENT_ROLE    = CURRENT_ROLE;
+  new.Log_SESSION_ID      = CURRENT_CONNECTION;
+  new.Log_TRANSACTION_ID  = CURRENT_TRANSACTION;
+  new.Log_ISOLATION_Mode  = RDB$GET_CONTEXT('SYSTEM', 'ISOLATION_LEVEL');
+  new.Log_REMOTE_PROCESS  = (SELECT MON$Remote_Process FROM MON$Attachments WHERE MON$ATTACHMENT_ID = CURRENT_CONNECTION);
+  new.usr_msg             = RDB$Get_Context('USER_TRANSACTION','LIB$Transactions_Log_usr_msg');
+END
+^
+
+CREATE OR ALTER TRIGGER LIB$TR_Monitor_Start INACTIVE ON TRANSACTION START POSITION 1
+AS
+DECLARE gn BIGINT;
+BEGIN
+  -- Just to pass Elekt Labs replication check
+  -- IF(Rdb$Get_Context('USER_SESSION','DatabaseReplicationFlag') IS NOT NULL) THEN EXIT;
+  gn = GEN_ID(LIB$Transactions_Log_Counter,-1);
+  IF(gn>0)THEN
+    RDB$Set_Context('USER_TRANSACTION','LIB$Transactions_Log_bDateUTC',GetExactTimestampUTC());
+END
+^
+
+CREATE OR ALTER TRIGGER LIB$TR_Monitor_Commit INACTIVE ON TRANSACTION COMMIT POSITION 32761
+AS
+BEGIN
+  IF(RDB$Get_Context('USER_TRANSACTION','LIB$Transactions_Log_bDateUTC') IS NOT NULL)THEN
+    INSERT INTO LIB$Transactions_Log(Rollbacked) VALUES(0);
+END
+^
+
+CREATE OR ALTER TRIGGER LIB$TR_Monitor_Rollback INACTIVE ON TRANSACTION ROLLBACK POSITION 32761
+AS
+BEGIN
+  IF(RDB$Get_Context('USER_TRANSACTION','LIB$Transactions_Log_bDateUTC') IS NOT NULL)THEN
+    IN AUTONOMOUS TRANSACTION DO
+      INSERT INTO LIB$Transactions_Log(Rollbacked) VALUES(1);
+END
+^
 
 SET TERM ;^ 
