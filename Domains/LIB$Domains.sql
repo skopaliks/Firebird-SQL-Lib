75496bff210dee364f6642831a4fe53d06aa5167
skopaliks <skopalik@elektlabs.cz>
2017-09-15 13:28:14 +0200

    Added domain for large UTF8 text

--- Domains/LIB$Domains.sql
+++ Domains/LIB$Domains.sql
@@ -7,6 +7,7 @@
 * Purpose : Domain types for general usage
 *
 * Revision History
+* 2017-09-15 - S.Skopalik: LIB$LargeText added
 ******************************************************************************/
 
 -- because create or alter domain doesn't exist
@@ -16,6 +17,12 @@ DECLARE ds VARCHAR(500);
 BEGIN
 -- Domain for UTC timestamps
   ds = 'Create Domain Lib$TimestampUTC As Timestamp;';
+  BEGIN
+    EXECUTE STATEMENT ds;
+  WHEN ANY DO BEGIN END
+  END
+-- Domain for larger UTF8 texts (like SQL DDL)
+  ds = 'CREATE DOMAIN LIB$LargeText AS BLOB sub_type 1 segment size 2048 CHARACTER SET UTF8;';
   BEGIN
     EXECUTE STATEMENT ds;
   WHEN ANY DO BEGIN END
