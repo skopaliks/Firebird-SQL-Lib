c1317688f8075bd561127401781b4d30af5bb765
skopaliks <skopalik@elektlabs.cz>
2017-09-17 13:57:00 +0200

    Added FB2.5 boolean and trick for CRLF thats avoids UDF

--- Domains/LIB$Domains.sql
+++ Domains/LIB$Domains.sql
@@ -15,14 +15,28 @@ SET TERM ^;
 EXECUTE BLOCK AS
 DECLARE ds VARCHAR(500);
 BEGIN
+-- CRLF constant
+-- use this way: DECLARE CRLF      LIB$CRLF;
+-- In code like: DDL = DDL||CRLF; 
+  ds = 'CREATE DOMAIN LIB$CRLF AS CHAR(2) DEFAULT x''0D0A''';
+  BEGIN
+    EXECUTE STATEMENT ds;
+  WHEN ANY DO BEGIN END
+  END  
 -- Domain for UTC timestamps
-  ds = 'Create Domain Lib$TimestampUTC As Timestamp;';
+  ds = 'CREATE DOMAIN Lib$TimestampUTC As Timestamp;';
   BEGIN
     EXECUTE STATEMENT ds;
   WHEN ANY DO BEGIN END
   END
 -- Domain for larger UTF8 texts (like SQL DDL)
   ds = 'CREATE DOMAIN LIB$LargeText AS BLOB sub_type 1 segment size 2048 CHARACTER SET UTF8;';
+  BEGIN
+    EXECUTE STATEMENT ds;
+  WHEN ANY DO BEGIN END
+  END
+-- Lib$BooleanF for FB2.5 boolean fields
+  ds = 'Create Domain LIB$BooleanF As Smallint Default 0 Check (value BETWEEN 0 AND 1);';
   BEGIN
     EXECUTE STATEMENT ds;
   WHEN ANY DO BEGIN END
