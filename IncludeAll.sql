3594ecc96c5e1a78353b092ce7c57579b26d6566
skopaliks <skopalik@elektlabs.cz>
2018-10-23 15:07:48 +0200

    Fixed problem with transaction deadlock

--- IncludeAll.sql
+++ IncludeAll.sql
@@ -5,16 +5,22 @@ INPUT Domains\LIB$Domains.sql;
 -- Best practies
 INPUT DB_Checks\LIB$CheckTriggersPossitions.sql;
 COMMIT;
+
 -- Monitoring
 INPUT Utils\LIB$TR_Monitor.sql;
 COMMIT;
+
 -- DDL
 INPUT DDL\LIB$DDL_Exception.sql;
 INPUT DDL\LIB$DDL_DropUnq.sql;
 INPUT DDL\LIB$DDL_Get_FK_Info.sql;
+COMMIT;
+
 -- IBReplicator
 INPUT IBReplicator\REPL$DBConnect.sql;
 INPUT IBReplicator\REPL$DDL.sql;
+COMMIT;
+
 -- DB compare
 INPUT Comparer\LIB$CMP_Exception.sql;
 INPUT Comparer\LIB$CMP_GetUserType.sql;
