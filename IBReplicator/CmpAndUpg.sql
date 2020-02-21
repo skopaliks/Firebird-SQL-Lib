20701d3569bce0f7267d2ec7eb0c549eec75ad11
skopaliks <30983584+skopaliks@users.noreply.github.com>
2020-02-21 14:23:06 +0100

    Handling NOT NULL

--- IBReplicator/CmpAndUpg.sql
+++ IBReplicator/CmpAndUpg.sql
@@ -2,10 +2,11 @@ SET TERM ^;
 EXECUTE BLOCK
 RETURNS(line VARCHAR(200))
 AS
-DECLARE db  VARCHAR(500);
-DECLARE tbl VARCHAR(128);
-DECLARE fld VARCHAR(128);
-DECLARE fsd VARCHAR(200);
+DECLARE db    VARCHAR(500);
+DECLARE tbl   VARCHAR(128);
+DECLARE fld   VARCHAR(128);
+DECLARE fsd   VARCHAR(200);
+DECLARE fnull SMALLINT;
 BEGIN
   db = RDB$GET_CONTEXT('USER_SESSION','DB_Target');
   line = '-- Generated at '||LOCALTIMESTAMP;
@@ -13,9 +14,11 @@ BEGIN
   line = '-- Target DB:'||db;
   SUSPEND;
   -- Add Missing fields into existing tables
-  FOR SELECT TRIM(Table_Name), TRIM(Field_Name), TRIM(Field_Source) FROM  LIB$CMP_Tables(:db, 'sysdba', 'masterkey')
+  FOR SELECT TRIM(Table_Name), TRIM(Field_Name), TRIM(Field_Source), Field_Null FROM  LIB$CMP_Tables(:db, 'sysdba', 'masterkey')
   WHERE IsFieldMissing = 1 AND IsTableMissing = 0
-  INTO tbl, fld, fsd DO BEGIN
+  INTO tbl, fld, fsd, fnull DO BEGIN
+    line = '';
+    SUSPEND;
     line = 'INSERT INTO Repl$DDL(SQL)SELECT ''ALTER TABLE '||tbl||' ADD '||fld||' '||fsd||''' FROM rdb$database';
     SUSPEND;
     line = '  WHERE NOT EXISTS(SELECT * FROM Rdb$Relation_Fields RF WHERE RF.rdb$Relation_Name = '''||tbl||''' AND RF.RDB$FIELD_NAME = '''||fld||''');'; 
@@ -26,7 +29,15 @@ BEGIN
     SUSPEND;
     line = 'SELECT * FROM Repl$AddOrUpdateRelation('''||tbl||''',NULL); COMMIT;';
     SUSPEND;
-    line = 'EXECUTE PROCEDURE MASA$UpdateBussinesObjects(UPPER('''||tbl''')); COMMIT;';
+    IF(fnull>0)THEN BEGIN
+      line = '-- Add code to set field to NOT NULL';
+      SUSPEND;
+      line = 'SELECT * FROM Repl$WaitForRound(500); COMMIT;';
+      SUSPEND;
+      line = 'EXECUTE PROCEDURE MASA$Set_Null_Flag('''||tbl||''', '''||fld||''', 1); COMMIT;';
+      SUSPEND;
+    END
+    line = 'EXECUTE PROCEDURE MASA$UpdateBussinesObjects(UPPER('''||tbl||''')); COMMIT;';
     SUSPEND;
   END 
 END
