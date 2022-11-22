a86439feeee2e82d5e5ca9b3a6c1fea816a30829
skopaliks <30983584+skopaliks@users.noreply.github.com>
2022-11-22 02:56:35 +0100

    update table records with empty trigger
    Added possibility to update table records with empty trigger in replication mode, add TRIM for improve output quality

--- Comparer/LIB$CMP_ExtractTrigger.sql
+++ Comparer/LIB$CMP_ExtractTrigger.sql
@@ -1,5 +1,5 @@
  /******************************************************************************
-* Stored Procedure : MASA$ExtractProcedure
+* Stored Procedure : LIB$CMP_ExtractTrigger
 *
 * Date    : 2018-06-29
 * Author  : Jaroslav Kejnar
@@ -8,7 +8,9 @@
 *
 * Revision History
 * ================
-* 2020-04-30 S.Skopalik - Fixed too many spaces in trigger comments 
+* 2020-04-30 S.Skopalik  - Fixed too many spaces in trigger comments
+* 2022-11-22 S.Skopalik  - Added possibility to update table records with empty trigger
+*                          in replication mode, add TRIM for improve output quality
 ******************************************************************************/
 SET TERM ^;
 
@@ -35,13 +37,13 @@ BEGIN
   -- Extract trigger header
   DDL = 'CREATE OR ALTER TRIGGER '|| TRIM(TriggerName);
   SELECT RDB$Trigger_Type, RDB$Trigger_Inactive, RDB$Trigger_Sequence FROM RDB$Triggers WHERE RDB$Trigger_Name = :TriggerName
-    INTO :trigger_type, :trigger_inactive, :trigger_sequence;
-  IF(trigger_type IS NULL) THEN EXCEPTION LIB$CMP_Exception 'Trigger ''' || :TriggerName || ''' not found.';
+    INTO trigger_type, trigger_inactive, trigger_sequence;
+  IF(trigger_type IS NULL) THEN EXCEPTION LIB$CMP_Exception 'Trigger ''' || TRIM(TriggerName) || ''' not found.';
   IF(trigger_inactive = 1) THEN
     DDL = DDL || ' INACTIVE ';
   IF(trigger_type IN (8192, 8193, 8194, 8195)) THEN
     DDL = DDL || (SELECT TriggerType FROM LIB$CMP_GetTriggerType(:trigger_type));
-  ELSE
+   ELSE
      DDL = DDL || ' FOR ' || (SELECT TRIM(RDB$Relation_Name) FROM RDB$Triggers WHERE RDB$Trigger_Name = :TriggerName) || ' ' || (SELECT TriggerType FROM LIB$CMP_GetTriggerType(:trigger_type));
   IF(trigger_sequence > 0) THEN
     DDL = DDL || ' POSITION ' || trigger_sequence;
@@ -56,7 +58,11 @@ BEGIN
     DDL = DDL || Source;
   END
   ELSE
-    DDL = DDL || 'AS' || CRLF || 'BEGIN' || CRLF || ' EXCEPTION LIB$CMP_Exception ''Trigger '  || :TriggerName || ' is not implemented'';' || CRLF || 'END';
+    DDL = DDL || 'AS' || CRLF || 
+    'BEGIN' || CRLF ||
+    '  IF(Rdb$Get_Context(''USER_SESSION'',''DatabaseReplicationFlag'') IS NOT NULL) THEN EXIT;' || CRLF || 
+    '  EXCEPTION LIB$CMP_Exception ''Trigger '  || TRIM(TriggerName) || ' is not implemented'';' || CRLF || 
+    'END';
   IsBody = 1;
   SUSPEND;
   IsBody = 0;
@@ -76,9 +82,9 @@ BEGIN
   isDrop = 0;
 
   -- Extract trigger comment
-  IF(:dsc IS NOT NULL)THEN BEGIN
+  IF(dsc IS NOT NULL)THEN BEGIN
     IsComment = 1;
-    DDL = 'COMMENT ON TRIGGER ' || TRIM(:TriggerName) || ' IS ''' || :dsc || '''';
+    DDL = 'COMMENT ON TRIGGER ' || TRIM(TriggerName) || ' IS ''' || dsc || '''';
     SUSPEND;
   END
   IsComment = 0;
