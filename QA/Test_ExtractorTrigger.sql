16faae6529eedcd0309edd4ddb6d265ea71274c1
skopaliks <30983584+skopaliks@users.noreply.github.com>
2023-09-29 23:44:53 +0200

    Fix false test fail

--- QA/Test_ExtractorTrigger.sql
+++ QA/Test_ExtractorTrigger.sql
@@ -5,20 +5,21 @@ SET TERM ^;
 EXECUTE BLOCK 
 AS
 DECLARE CRLF  LIB$CRLF;
-DECLARE v_ddl BLOB;
+DECLARE v_ddl LIB$LargeText;
 DECLARE t_header VARCHAR(1000);
 DECLARE t_source VARCHAR(2000);
 DECLARE t_dsc VARCHAR(1000);
 BEGIN
-  t_header = 'CREATE OR ALTER TRIGGER TEST_TRIGGER_BIU FOR CMP_TEST BEFORE INSERT OR UPDATE POSITION 33';
+  t_header = 'CREATE OR ALTER TRIGGER TEST_TRIGGER_BIU FOR CMP_TEST ACTIVE BEFORE INSERT OR UPDATE POSITION 33';
   t_source = 'AS' || CRLF || 'BEGIN' || CRLF || '  --some comment for test in code' || CRLF
             || '  INSERT INTO CMP_TEST VALUES(3);' || CRLF || 'END';
   t_dsc = 'COMMENT ON TRIGGER TEST_TRIGGER_BIU IS ''test trigger comment''';
   EXECUTE STATEMENT t_header || CRLF || t_source;
   EXECUTE STATEMENT t_dsc;
-  IF(t_header <> (SELECT CAST(ddl AS VARCHAR(1000)) FROM LIB$CMP_ExtractTrigger('TEST_TRIGGER_BIU', 0) WHERE isHeader = 1)) THEN
-    EXCEPTION LIB$CMP_EXCEPTION 'Extracted trigger header is different';
-  IF(t_source <> (SELECT CAST(ddl AS VARCHAR(2000)) FROM LIB$CMP_ExtractTrigger('TEST_TRIGGER_BIU', 0) WHERE isSource = 1)) THEN
+  v_ddl = (SELECT CAST(ddl AS VARCHAR(1000)) FROM LIB$CMP_ExtractTrigger('TEST_TRIGGER_BIU', 0) WHERE isHeader = 1);
+  IF(t_header <> v_ddl) THEN
+    EXCEPTION LIB$CMP_EXCEPTION 'Extracted trigger header is different ('||v_ddl||')';
+  IF(t_source <> (SELECT CAST(ddl AS VARCHAR(2000)) FROM LIB$CMP_ExtractTrigger('TEST_TRIGGER_BIU', 0) WHERE isBody = 1)) THEN
     EXCEPTION LIB$CMP_EXCEPTION 'Extracted trigger source is different';
   IF(t_dsc <> (SELECT CAST(ddl AS VARCHAR(1000)) FROM LIB$CMP_ExtractTrigger('TEST_TRIGGER_BIU', 0) WHERE isComment = 1)) THEN
     EXCEPTION LIB$CMP_EXCEPTION 'Extracted trigger comment is different';
